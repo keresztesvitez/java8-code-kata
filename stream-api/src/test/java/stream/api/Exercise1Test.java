dced1e6c0d95f66bcdbe578212829a3d3c68e41b
Keresztes Adam <keresztesvitez@gmail.com>
2017-09-05 08:40:10 +0200

    Stream, predicate, filter, map

--- stream-api/src/test/java/stream/api/Exercise1Test.java
+++ stream-api/src/test/java/stream/api/Exercise1Test.java
@@ -45,8 +45,8 @@ public class Exercise1Test extends ClassicOnlineStore {
          * Use method reference(best) or lambda expression(okay) for creating {@link Function} which will
          * convert {@link Customer} to {@link Integer}, and then apply it by using {@link Stream#map}.
          */
-        Function<Customer, Integer> getAgeFunction = null;
-        Stream<Integer> ageStream = null;
+        Function<Customer, Integer> getAgeFunction = Customer::getAge;
+        Stream<Integer> ageStream = customerList.stream().map(getAgeFunction);
 
         assertTrue(AssertUtil.isLambda(getAgeFunction));
         List<Integer> ages = ageStream.collect(Collectors.toList());
